dd500cfef248de7fe253f685194c365c115c0d3b
Zebity Spring <zebity@yahoo.com>
2023-05-14 18:25:31 +1000

    Test if_volume block

--- helpers/if_volume.js
+++ helpers/if_volume.js
@@ -6,40 +6,59 @@
 
 const {metaData} = require('../services/proxy');
 const {SafeString} = require('../services/handlebars');
+const _ = require('lodash');
 
 const {getMetaDataUrl} = metaData;
 
-module.exports = function if_volume(volstd, tst) {
+module.exports = function if_volume(...attrs) {
+  // module.exports = function if_volume(volstd, tst) {
+  const options = attrs.pop();
+  const isBlock = _.has(options, 'fn');
+ 
   let res = false;
   let i = 0;
-  let outputUrl = getMetaDataUrl(this, false);
 
+  if (_.isEmpty(attrs)) {
+    // Just return false
+  } else if (attrs.length === 2) {
 
-  if (outputUrl == '/') {
-    // main page
-    res = tst === 1 || tst === 'main' || tst === 'i';
-  } else {
-    let urlbits = outputUrl.split('/');
-    let arr = JSON.parse(volstd);
+    let volstd = attrs[0];
+    let tst = attrs[1];
+    let outputUrl = getMetaDataUrl(this, false);
 
-    if (arr != null && arr != undefined) {
-      for (i = 0; i < arr.length; i++) {
-        if (arr[i][0] === urlbits[1]) {
-          switch (i) {
-            case 0: res = tst === 2 || tst === 'ii';
-                    break;
-            case 1: res = tst === 3 || tst === 'iii';
-                    break;
-            case 2: res = tst === 4 || tst === 'iv';
-                    break;
-            case 3: res = tst === 5 || tst === 'v';
-                    break;
+    if (outputUrl == '/') {
+      // main page
+      res = tst === 1 || tst === 'main' || tst === 'i';
+    } else {
+      let urlbits = outputUrl.split('/');
+      let arr = JSON.parse(volstd);
+
+      if (arr != null && arr != undefined) {
+        for (i = 0; i < arr.length; i++) {
+          if (arr[i][0] === urlbits[1]) {
+            switch (i) {
+              case 0: res = tst === 2 || tst === 'ii';
+                      break;
+              case 1: res = tst === 3 || tst === 'iii';
+                      break;
+              case 2: res = tst === 4 || tst === 'iv';
+                      break;
+              case 3: res = tst === 5 || tst === 'v';
+                      break;
+            }
+            break;
           }
-          break;
         }
       }
     }
-  }
 
+    if (isBlock) {
+      if (res) {
+        return options.fn(this);
+      } else {
+        return options.inverse(this);
+      }
+    }
+  }
   return res;
 };
